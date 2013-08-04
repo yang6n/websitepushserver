e7619cd0c183d5e85d8bbfabdd8e284b20a83c75
yang6n <yang6n@gmail.com>
2013-08-04 00:21:55 -0500

    Add log api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express' });
+};
+
+exports.log = function (req, res) {
+    console.log("log: " + JSON.stringify(req.body));
+    res.send(200);
 };
